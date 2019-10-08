9c209475c9ccfebd73ba0eac2a9c4917d1ce9eb8
Iker Reyes <ikerreyes@users.noreply.github.com>
2019-10-08 20:30:18 +0200

    Show todo directives in the output

--- source/conf.py
+++ source/conf.py
@@ -33,6 +33,9 @@ extensions = [
     'IPython.sphinxext.ipython_console_highlighting'
 ]
 
+# If true, `todo` and `todoList` produce output, else they produce nothing.
+todo_include_todos = True
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
